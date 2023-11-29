69ef64c8d15cb8740c77f66296ab2b95c89b054c
Juan Da Don <_quentin_@live.com>
2023-11-29 00:58:36 -0500

    Update requirements.txt and setup.py requirements to include sqlalchemy as we begin to add support for duplicate avoidance.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='coomer_xtractor',
-    version='0.0.3',
+    version='0.0.4',
     packages=['coomer_xtractor', 'coomer_xtractor.menus', 'coomer_xtractor.models', 'coomer_xtractor.actions',
               'coomer_xtractor.scrapers'],
     url='https://github.com/taux1c/coomer_xtractor/',
@@ -19,7 +19,8 @@ setup(
         "aiofiles~=23.2.1",
         "httpx~=0.25.2",
         "bs4~=0.0.1",
-        "beautifulsoup4~=4.12.2"
+        "beautifulsoup4~=4.12.2",
+        "SQLAlchemy~=2.0.23",
     ],
     long_description="""Coomer Xtractor is a simple tool used to download content from coomer.su. The tool is freely available for use, and contributions from the community are welcomed and appreciated.""",
 
