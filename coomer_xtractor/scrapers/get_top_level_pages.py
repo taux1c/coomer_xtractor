1679242532cef6b67c433c6fec7268d3413a3110
Taux1c <24677318+taux1c@users.noreply.github.com>
2024-02-28 18:59:55 -0500

    Update get_top_level_pages.py
    Update to the get_top_level_pages.py due to the removal of some parts of coomer.

--- coomer_xtractor/scrapers/get_top_level_pages.py
+++ coomer_xtractor/scrapers/get_top_level_pages.py
@@ -8,12 +8,8 @@ async def get_pages(url):
     async with AsyncClient(follow_redirects=True, headers=headers) as client:
         r = await client.get(url)
         soup = BeautifulSoup(r.text, 'lxml')
-        last_page_count = int(soup.find('a', string=">>")['href'].split('o=')[-1])
-        pages = last_page_count/50
-        base = 0
-        urls = []
-        urls.append(url)
-        for i in range(int(pages)):
-            base += 50
-            urls.append(f"{url}?o={base}")
+        link_list = []
+        for link in soup.select('menu a[href]'):
+            link_list.append('https://coomer.su' + link['href'])
+        urls = list(set(link_list))
         return urls
